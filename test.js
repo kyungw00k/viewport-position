f654c2d3959fb292f5937f81b39ec955e62006c1
Kyungwook Park <parksama@gmail.com>
2016-07-28 18:54:51 +0900

    Use mocha instead of tape

--- test.js
+++ test.js
@@ -1,34 +1,58 @@
+/* global describe, beforeEach, afterEach, it */
+
 'use strict'
 
-var test = require('tape')
+var assert = require('assert')
 var viewportPosition = require('./')
 
-test('viewport position', function (t) {
-  document.body.style.position = 'relative'
-  document.body.style.width = '30000px'
-  document.body.style.height = '30000px'
+describe('viewport position', function () {
+  var win = window
+  var doc = win.document
 
-  window.scrollTo(0, 0)
-
-  t.deepEqual(viewportPosition(), {
-    top: 0,
-    right: 400,
-    left: 0,
-    bottom: 300,
-    width: 400,
-    height: 300
+  before(function () {
+    document.body.style.padding = '0'
+    document.body.style.margin = '0'
+    document.body.style.position = 'relative'
+    document.body.style.width = '30000px'
+    document.body.style.height = '30000px'
   })
 
-  window.scrollTo(300, 300)
+  it('scroll to (0, 0)', function (done) {
+    var scrollTop = 0
+    var scrollLeft = 0
+    var windowWidth = doc.documentElement.clientWidth || doc.body.clientWidth
+    var windowHeight = doc.documentElement.clientHeight || doc.body.clientHeight
 
-  t.deepEqual(viewportPosition(), {
-    top: 300,
-    right: 700,
-    left: 300,
-    bottom: 600,
-    width: 400,
-    height: 300
+    setTimeout(function () {window.scrollTo(scrollLeft, scrollTop)}, 1)
+
+    setTimeout(function () {
+      var pos = viewportPosition()
+
+      assert.equal(pos.top, scrollTop)
+      assert.equal(pos.left, scrollLeft)
+      assert.equal(pos.right, scrollLeft + windowWidth)
+      assert.equal(pos.bottom, scrollTop + windowHeight)
+
+      done()
+    }, 100)
   })
 
-  t.end()
+  it('scroll to (300,300)', function (done) {
+    var scrollTop = 300
+    var scrollLeft = 300
+    var windowWidth = doc.documentElement.clientWidth || doc.body.clientWidth
+    var windowHeight = doc.documentElement.clientHeight || doc.body.clientHeight
+
+    setTimeout(function () {window.scrollTo(scrollLeft, scrollTop)}, 1)
+
+    setTimeout(function () {
+      var pos = viewportPosition()
+
+      assert.equal(pos.top, scrollTop)
+      assert.equal(pos.left, scrollLeft)
+      assert.equal(pos.right, scrollLeft + windowWidth)
+      assert.equal(pos.bottom, scrollTop + windowHeight)
+      done()
+    }, 100)
+  })
 })
